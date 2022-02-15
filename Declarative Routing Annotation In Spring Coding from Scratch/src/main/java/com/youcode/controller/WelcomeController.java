150112ca6e8e06ecfee50ff854c748c91f33f42b
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-02-15 15:15:31 +0100

    Declarative Routing Annotation In Spring Coding from Scratch

--- Declarative Routing Annotation In Spring Coding from Scratch/src/main/java/com/youcode/controller/WelcomeController.java
+++ Declarative Routing Annotation In Spring Coding from Scratch/src/main/java/com/youcode/controller/WelcomeController.java
@@ -1,25 +1,30 @@
 package com.youcode.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/user") // prefix all request mapping on this controller with "/user"
 public class WelcomeController {
 
     @RequestMapping("/")
     public String Welcome(){
         return "welcome";
     }
-    @RequestMapping(value = "/signup" ,method = RequestMethod.GET)
+//    @RequestMapping(value = "/signup" ,method = RequestMethod.GET)
+    @GetMapping("/signup")
     public String singUp(){
-        System.out.println("GET method");
+        System.out.println("GET method called");
         return "SignUpForm";
     }
 
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+//    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    @PostMapping("/signup")
     public String singUpPost(){
-        System.out.println("POST method");
+        System.out.println("POST method called");
         return "SignUpForm";
     }
 }
